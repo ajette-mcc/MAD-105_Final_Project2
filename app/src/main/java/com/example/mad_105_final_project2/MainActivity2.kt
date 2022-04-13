7e9a0f56e438fabf0766a9a80c788c923819b21c
Al Jette <ajette@students.mchenry.edu>
2022-04-13 15:17:36 -0500

    Screen 1 with code and screen 2 Display mostly complete, need to collect information from screen 2 input and pass to screen 3 (screen 3 with skeleton display and code)

--- app/src/main/java/com/example/mad_105_final_project2/MainActivity2.kt
+++ app/src/main/java/com/example/mad_105_final_project2/MainActivity2.kt
@@ -1,10 +1,11 @@
 package com.example.mad_105_final_project2
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.SeekBar
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity2 : AppCompatActivity() {
 
@@ -17,6 +18,37 @@ class MainActivity2 : AppCompatActivity() {
 
         val displayName: TextView = findViewById(R.id.idPrintName)
         val displayPhone: TextView = findViewById(R.id.idPrintPhone)
+        val custSelectTeaSize: SeekBar = findViewById(R.id.idSeekBarSize)
+        val custSelectTeaSizeText: TextView = findViewById(R.id.idSeekBarText)
+        var custSelectedTeaSize: Int = 2
+
+
+        //  When user moves the Seekbar, retrieve new value and update seekbarOutputText
+        custSelectTeaSize.run {
+
+            //  When user moves the Seekbar, retrieve new value and update seekbarOutputText
+            setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                    custSelectedTeaSize = p1
+                    when (custSelectedTeaSize) {
+                        1 -> custSelectTeaSizeText.text = "Small"
+                        2 -> custSelectTeaSizeText.text = "Medium"
+                        3 -> custSelectTeaSizeText.text = "Large"
+                        else -> custSelectTeaSizeText.text = "Out of Bound"
+                    }
+                }
+
+                override fun onStartTrackingTouch(p0: SeekBar?) {
+                    //                     seekbarOutputText.text = "TinyFine"
+                }
+
+                override fun onStopTrackingTouch(p0: SeekBar?) {
+
+                }
+
+            })
+        }
+
         val screen3Btn = findViewById<Button>(R.id.idButtonToScr3)
 
         val extras = intent.extras
