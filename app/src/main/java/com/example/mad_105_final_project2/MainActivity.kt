c908aba96d54955fecbb099b00055fb2028a6ea5
Al Jette <ajette@students.mchenry.edu>
2022-04-19 10:31:54 -0500

    Screen 1 with code and error checking Screen 2 Display complete, need to collect information from screen 2 input and pass to screen 3 (screen 3 with skeleton display and code)

--- app/src/main/java/com/example/mad_105_final_project2/MainActivity.kt
+++ app/src/main/java/com/example/mad_105_final_project2/MainActivity.kt
@@ -1,10 +1,27 @@
 package com.example.mad_105_final_project2
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+/*********************************************************************************
+ * MAD-105 Final Project to get user input on a cup of Tea (ie: similar to Starbucks, but with tea)
+ * Author:  Al Jette
+ * Date: April-2022
+ * First Screen (MainActivity) collects Customer Name & Phone #
+ * Second Screen (MainActivity2) collects details on the cup of tea
+ * Third Screen (MainActivity3) displays the customer info and tea ordered
+ * Fourth Screen (MainActivity4) says "Thank you for your order"
+ *
+ * First Screen Variables
+ *   idCustName (from layout) = customerName => the customer name
+ *   idCustPhone (from layout) = customerPhone => the customer's phone
+ *   idButtonToScr2 (from layout) = screen2Btn => goes to screen 2 (MainActivity2)
+ *
+ *   Software checks to ensure customerName and customerPhone are NOT Blank in the routine checkData
+ */
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,12 +32,38 @@ class MainActivity : AppCompatActivity() {
         val customerPhone = findViewById<TextView>(R.id.idCustPhone)
         val screen2Btn = findViewById<Button>(R.id.idButtonToScr2)
 
+        // If user clicks button to move to screen2 and if checkData routine says data is good (True)
         screen2Btn.setOnClickListener {
-            val nextScreen = Intent (this@MainActivity, MainActivity2::class.java)
-            nextScreen.putExtra("CustName", customerName.text.toString())
-            nextScreen.putExtra( "CustPhone", customerPhone.text.toString())
+            if (checkData()) {
+                val nextScreen = Intent(this@MainActivity, MainActivity2::class.java)
+                // pass customerName and customerPhone to MainActivity2 and start MainActivity2
+                nextScreen.putExtra("CustName", customerName.text.toString())
+                nextScreen.putExtra("CustPhone", customerPhone.text.toString())
 
-            startActivity(nextScreen)
+                startActivity(nextScreen)
+            }
         }
     }
+    private fun checkData(): Boolean {
+        val customerName = findViewById<TextView>(R.id.idCustName)
+        val customerPhone = findViewById<TextView>(R.id.idCustPhone)
+
+        if (customerName.text.toString().isEmpty()) {
+            // return error, want at least some string for the customer name
+            customerName.error = "Invalid Name"
+            customerName.requestFocus()
+            return false
+        }
+
+        if (customerPhone.text.toString().isEmpty()) {
+            // return error, want at least some string for the phone number
+            customerPhone.error = "Invalid Name"
+            customerPhone.requestFocus()
+            return false
+        }
+
+        return true
+
+    }
+
 }
--- app/src/main/java/com/example/mad_105_final_project2/MainActivity2.kt
+++ app/src/main/java/com/example/mad_105_final_project2/MainActivity2.kt
@@ -3,9 +3,32 @@ package com.example.mad_105_final_project2
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.RadioButton
 import android.widget.SeekBar
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+
+/*********************************************************************************
+ * MAD-105 Final Project to get user input on a cup of Tea (ie: similar to Starbucks, but with tea)
+ * Author:  Al Jette
+ * Date: April-2022
+ * Second Screen (MainActivity2) collects details on the cup of tea
+ *
+ * First Screen Variables passed to MainActivity2
+ *   customerName => the customer name
+ *   customerPhone => the customer's phone
+ *
+ * Second Screen Variables
+ *   idBtnBlackTea (from layout) = teaTypeBlack
+ *   idBtnGreenTea (from layout) = teaTypeGreen
+ *   idBtnHerbalTea (from layout) = teaTypeHerbal
+ *   idToggleSugar (from layout)
+ *   idToggleCream (from layout)
+ *
+ *   idSeekBarSize (from layout) = custSelectTeaSize:  Tea size being ordered 1=small, 2=medium, 3=large
+ *   idSeekBarText (from layout) =custSelectTeaSizeText: Displays the tea size selected by the customer
+ *   idButtonToScr3 (from layout) = screen3Btn => goes to screen 3 (MainActivity3)
+ */
 
 class MainActivity2 : AppCompatActivity() {
 
@@ -18,10 +41,32 @@ class MainActivity2 : AppCompatActivity() {
 
         val displayName: TextView = findViewById(R.id.idPrintName)
         val displayPhone: TextView = findViewById(R.id.idPrintPhone)
+        val rbTeaBlack = findViewById<RadioButton>(R.id.idBtnBlackTea)
+        val rbTeaGreen = findViewById<RadioButton>(R.id.idBtnGreenTea)
+        val rbTeaHerbal = findViewById<RadioButton>(R.id.idBtnHerbalTea)
         val custSelectTeaSize: SeekBar = findViewById(R.id.idSeekBarSize)
         val custSelectTeaSizeText: TextView = findViewById(R.id.idSeekBarText)
-        var custSelectedTeaSize: Int = 2
+        val screen3Btn = findViewById<Button>(R.id.idButtonToScr3)
 
+        val extras = intent.extras
+        if (extras != null) {
+            customerName = extras.getString("CustName")
+            customerPhone = extras.getString("CustPhone")
+        }
+
+        displayName.text = customerName
+        displayPhone.text = customerPhone
+
+        var custSelectedTeaSize: Int = 2
+        var teaType = "None"
+
+        if (rbTeaBlack.isChecked) {
+            teaType = "Black"
+        } else if (rbTeaGreen.isChecked) {
+            teaType = "Green"
+        } else if (rbTeaHerbal.isChecked) {
+            teaType = "Herbal"
+        }
 
         //  When user moves the Seekbar, retrieve new value and update seekbarOutputText
         custSelectTeaSize.run {
@@ -49,16 +94,6 @@ class MainActivity2 : AppCompatActivity() {
             })
         }
 
-        val screen3Btn = findViewById<Button>(R.id.idButtonToScr3)
-
-        val extras = intent.extras
-        if (extras != null) {
-            customerName = extras.getString("CustName")
-            customerPhone = extras.getString("CustPhone")
-        }
-
-        displayName.text = customerName
-        displayPhone.text = customerPhone
 
         screen3Btn.setOnClickListener {
             val nextScreen = Intent(this@MainActivity2, MainActivity3::class.java)
